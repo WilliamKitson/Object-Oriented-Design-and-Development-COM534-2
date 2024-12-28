4dd7b96357b480f9bd51529d3bfceb6e983eff96
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-28 13:38:15 +0000

    SearchRoomsPage redefined

--- src/main/kotlin/SearchRoomsPage.kt
+++ src/main/kotlin/SearchRoomsPage.kt
@@ -1,6 +1,3 @@
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Button
@@ -10,11 +7,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 
 class SearchRoomsPage {
     private var buildings = listOf<String>()
@@ -47,6 +44,27 @@ class SearchRoomsPage {
                     Text("Search")
                 }
             }
+
+            val number = mutableListOf<String>()
+            val building = mutableListOf<String>()
+
+            transaction {
+                RoomsTable.selectAll().forEach {
+                    number += it[RoomsTable.number].toString()
+                    building += it[RoomsTable.building]
+                }
+            }
+
+            Row {
+                LazyColumn {
+                    items(number) { curItem -> Text(curItem) }
+                }
+                LazyColumn {
+                    items(building) { curItem -> Text(curItem) }
+                }
+            }
+
+
 
             Button (onClick = {}) {
                 Text("Add Room")
