9055035459ad446ddb52566c9a8d373fbef309ae
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-28 16:20:25 +0000

    AddRoomTest defined

---
+++ src/test/kotlin/AddRoomTest.kt
@@ -0,0 +1,33 @@
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class AddRoomTest {
+    @Test
+    fun testAddRoom() {
+        Database.connect(
+            "jdbc:sqlite:4kitsw10_COM534_2_database.db",
+            "org.sqlite.JDBC"
+        )
+
+        transaction {
+            RoomsTable.deleteAll()
+        }
+
+        val bookingSystem = BookingSystem()
+
+        for (i in 0..5) {
+            val input = Room(
+                i.toString(),
+                i.toString(),
+                i.toString(),
+                i * 5
+            )
+
+            bookingSystem.addRoom(input)
+            assert(bookingSystem.rooms[i] == input)
+        }
+    }
+}
