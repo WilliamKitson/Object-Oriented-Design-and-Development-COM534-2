2c53fe69af087ec1d4b04371bc6341e1f32343e4
4kitsw10 <4kitsw10@solent.ac.uk>
2024-12-28 14:32:23 +0000

    SearchRoomsPage redefined

--- src/main/kotlin/SearchRoomsPage.kt
+++ src/main/kotlin/SearchRoomsPage.kt
@@ -4,14 +4,12 @@ import androidx.compose.material.Button
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
 import org.jetbrains.exposed.sql.and
 
 class SearchRoomsPage {
@@ -38,87 +36,76 @@ class SearchRoomsPage {
     @Composable
     fun render() {
         Column {
+            var selectedBuilding by remember { mutableStateOf("") }
+
             Row {
-                renderDropdown(buildings)
-                renderDropdown(computerTypes)
-                Button (onClick = {}) {
-                    Text("Search")
+                val isDropDownExpanded = remember {
+                    mutableStateOf(false)
+                }
+
+                val itemPosition = remember {
+                    mutableStateOf(0)
+                }
+
+                Button(onClick = {
+                    isDropDownExpanded.value = true
+                }){
+                    Text(text = buildings[itemPosition.value])
+                }
+                DropdownMenu(
+                    expanded = isDropDownExpanded.value,
+                    onDismissRequest = {
+                        isDropDownExpanded.value = false
+                    }) {
+                    buildings.forEachIndexed { index, item ->
+                        DropdownMenuItem(
+                            onClick = {
+                                isDropDownExpanded.value = false
+                                itemPosition.value = index
+                                selectedBuilding = item
+                            }, content = {
+                                Text(text = item)
+                            }
+                        )
+                    }
                 }
             }
 
-            renderTable()
+            val number = mutableListOf("Room Number")
+            val building = mutableListOf("Building")
+            val computerType = mutableListOf("Computer Type")
+            val nComputers = mutableListOf("Number of Computers")
+
+            transaction {
+                RoomsTable.selectAll().where {
+                    RoomsTable.building.eq(selectedBuilding).and {
+                        RoomsTable.computerType.eq("Windows PC")
+                    }
+                }.forEach {
+                    number += it[RoomsTable.number].toString()
+                    building += it[RoomsTable.building]
+                    computerType += it[RoomsTable.computerType]
+                    nComputers += it[RoomsTable.nComputers].toString()
+                }
+            }
+
+            Row {
+                LazyColumn {
+                    items(number) { curItem -> Text(curItem) }
+                }
+                LazyColumn {
+                    items(building) { curItem -> Text(curItem) }
+                }
+                LazyColumn {
+                    items(computerType) { curItem -> Text(curItem) }
+                }
+                LazyColumn {
+                    items(nComputers) { curItem -> Text(curItem) }
+                }
+            }
 
             Button (onClick = {}) {
                 Text("Add Room")
-            }
-        }
-    }
-
-    @Composable
-    private fun renderDropdown(dropdownItems: List<String>) {
-        val isDropDownExpanded = remember {
-            mutableStateOf(false)
-        }
-
-        val itemPosition = remember {
-            mutableStateOf(0)
-        }
-
-        Button(onClick = {
-            isDropDownExpanded.value = true
-        }){
-            Text(text = dropdownItems[itemPosition.value])
-        }
-        DropdownMenu(
-            expanded = isDropDownExpanded.value,
-            onDismissRequest = {
-                isDropDownExpanded.value = false
-            }) {
-            dropdownItems.forEachIndexed { index, item ->
-                DropdownMenuItem(
-                    onClick = {
-                        isDropDownExpanded.value = false
-                        itemPosition.value = index
-                    }, content = {
-                        Text(text = item)
-                    }
-                )
-            }
-        }
-    }
-
-    @Composable
-    fun renderTable() {
-        val number = mutableListOf("Room Number")
-        val building = mutableListOf("Building")
-        val computerType = mutableListOf("Computer Type")
-        val nComputers = mutableListOf("Number of Computers")
-
-        transaction {
-            RoomsTable.selectAll().where {
-                RoomsTable.building.eq("Library").and {
-                    RoomsTable.computerType.eq("Windows PC")
-                }
-            }.forEach {
-                number += it[RoomsTable.number].toString()
-                building += it[RoomsTable.building]
-                computerType += it[RoomsTable.computerType]
-                nComputers += it[RoomsTable.nComputers].toString()
-            }
-        }
-
-        Row {
-            LazyColumn {
-                items(number) { curItem -> Text(curItem) }
-            }
-            LazyColumn {
-                items(building) { curItem -> Text(curItem) }
-            }
-            LazyColumn {
-                items(computerType) { curItem -> Text(curItem) }
-            }
-            LazyColumn {
-                items(nComputers) { curItem -> Text(curItem) }
             }
         }
     }
