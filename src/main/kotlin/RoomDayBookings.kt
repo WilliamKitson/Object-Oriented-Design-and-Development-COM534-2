66502f2b3633bd68a99c9054bfebab478e101c9a
4kitsw10 <4kitsw10@solent.ac.uk>
2025-01-01 16:43:42 +0000

    RoomDayBookings redefined

--- src/main/kotlin/RoomDayBookings.kt
+++ src/main/kotlin/RoomDayBookings.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import java.awt.print.Book
 
 class RoomDayBookings(private val connection: String, private val bookingSystem: BookingSystem) {
     @Composable
@@ -33,15 +32,11 @@ class RoomDayBookings(private val connection: String, private val bookingSystem:
                     LazyColumn(Modifier.fillMaxWidth().padding(16.dp)) {
                         item {
                             Row(Modifier.background(Color.Gray)) {
-                                TableCell(text = "Room", weight = columnWeight)
-                                TableCell(text = "Day", weight = columnWeight)
                                 TableCell(text = "Booking", weight = columnWeight)
                             }
                         }
                         items(bookings) {
                             Row(Modifier.fillMaxWidth()) {
-                                TableCell(text = "TEMP", weight = columnWeight)
-                                TableCell(text = "TEMP", weight = columnWeight)
                                 TableCell(text = it.toString(), weight = columnWeight)
                             }
                         }
