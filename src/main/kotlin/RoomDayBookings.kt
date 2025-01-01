9003effa054f6737fb6a738234657b28a7560334
4kitsw10 <4kitsw10@solent.ac.uk>
2025-01-01 16:28:49 +0000

    RoomDayBookings redefined

--- src/main/kotlin/RoomDayBookings.kt
+++ src/main/kotlin/RoomDayBookings.kt
@@ -1,10 +1,18 @@
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import java.awt.print.Book
 
 class RoomDayBookings(private val connection: String, private val bookingSystem: BookingSystem) {
     @Composable
@@ -14,6 +22,30 @@ class RoomDayBookings(private val connection: String, private val bookingSystem:
         NavHost(navController, startDestination = "roomDayBookings") {
             composable(route = "roomDayBookings") {
                 Column {
+                    val bookings = mutableListOf<Booking>()
+
+                    for (i in bookingSystem.getAllBookingsForRoomAndDay("JS001", "Monday")!!) {
+                        bookings += i
+                    }
+
+                    val columnWeight = .3f
+
+                    LazyColumn(Modifier.fillMaxWidth().padding(16.dp)) {
+                        item {
+                            Row(Modifier.background(Color.Gray)) {
+                                TableCell(text = "Room", weight = columnWeight)
+                                TableCell(text = "Day", weight = columnWeight)
+                                TableCell(text = "Booking", weight = columnWeight)
+                            }
+                        }
+                        items(bookings) {
+                            Row(Modifier.fillMaxWidth()) {
+                                TableCell(text = "TEMP", weight = columnWeight)
+                                TableCell(text = "TEMP", weight = columnWeight)
+                                TableCell(text = it.toString(), weight = columnWeight)
+                            }
+                        }
+                    }
                     Button(onClick = {
                         navController.navigate("adminPage")
                     }) {
@@ -25,5 +57,19 @@ class RoomDayBookings(private val connection: String, private val bookingSystem:
                 AdminPage(connection, bookingSystem).render()
             }
         }
+    }
+
+    @Composable
+    fun RowScope.TableCell(
+        text: String,
+        weight: Float
+    ) {
+        Text(
+            text = text,
+            Modifier
+                .border(1.dp, Color.Black)
+                .weight(weight)
+                .padding(8.dp)
+        )
     }
 }
